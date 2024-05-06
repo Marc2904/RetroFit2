f2190d9d316abc3ace97124bb08bde44e8d19570
marc <115090034+Marc2904@users.noreply.github.com>
2024-05-06 17:41:28 +0200

    new

---
+++ app/src/main/java/com/example/retrofit2/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.retrofit2
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_super_hero_list)
+
+    }
+
+}
